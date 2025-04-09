d593729d817f10f675feadb3e013a3bffbdfecc9
TheAnalyticsCareer <careeranalytics499@gmail.com>
2025-04-09 18:01:16 +0530

    updated code

--- routers/auth.js
+++ routers/auth.js
@@ -1,26 +1,3 @@
-// const express=require("express")
-// const router=express.Router()
-// const authControllers=require("../controllers/authentication");
-// const verify=require("../middleware/verify")
-
-
-// router.post("/signUp",authControllers.createNewUser);
-// router.post("/userLogin",authControllers.userLogin);
-// router.get("/viewUsers/:companyName",verify,authControllers.viewUsers)
-// router.get("/getUser/:companyName/:userId",verify,authControllers.getUser);
-// router.put("/updateUser/:companyName/:userId",verify,authControllers.updateUser)
-// router.delete("/deleteUser/:companyName/:userId",verify,authControllers.deleteUser)
-// // router.delete("/deleteCompany/:companyName",verify,authControllers.deleteCompany);
-
-// router.get("/searchUser/:companyName/:searchedText",verify,authControllers.searchUser)
-
-// module.exports=router;
-
-
-
-
-
-
 
 
 
--- routers/purchses.js
+++ routers/purchses.js
@@ -1,32 +1,3 @@
-// const express=require("express")
-// const router=express.Router()
-// const purchaseControllers=require("../controllers/purchase")
-// const verify=require("../middleware/verify")
-
-// router.post("/purchaseEntry/:companyName",verify,purchaseControllers.purchaseEntry);
-
-// // router.get("/viewPurchaseRecord/:companyName",verify,purchaseControllers.viewPurchaseRecord);
-
-// router.get("/viewPurchaseRecord/:companyName/:month/:year",verify,purchaseControllers.viewPurchaseRecord);
-
-// router.get("/viewPurchaseRecord/:companyName/:month/:year/:date",verify,purchaseControllers.viewPurchaseRecord);
-
-// router.get("/filterPurchaseRecord/:companyName/:purchase_type/:purchase_product/:purchase_person/:purchase_supplier/:created_at",verify,purchaseControllers.filterPurchaseRecord);
-
-// router.get("/getUniquePurchase/:companyName/:purchaseId",verify,purchaseControllers.viewUniquePurchaseRecord);
-
-// router.put("/updatePurchaseRecord/:companyName/:purchaseId",verify,purchaseControllers.updatePurchaseRecord);
-
-// router.delete("/deletePurchaseRecord/:companyName/:purchaseId",verify,purchaseControllers.deletePurchaseRecord);
-
-// router.get("/searchPurchaseRecord/:companyName/:searchedText",verify,purchaseControllers.searchPurchaseRecord);
-
-
-// module.exports=router;
-
-
-
-
 
 
 
